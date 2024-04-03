dae4d3491233279bd2bda02e7a0b62d0919f18eb
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-03 17:18:34 +0200

    init TrisGame with board structure

--- src/www/experis/bonus/TrisGame.java
+++ src/www/experis/bonus/TrisGame.java
@@ -4,11 +4,15 @@ public class TrisGame {
     public static void main(String[] args) {
         final int width = 3;
         final int heigth = 3;
-        for (int i = 0; i < width; i++) {
-            System.out.print(" " + (i+1));
-            for (int j = 0; j < heigth; j++) {
+        char[][] board = new char[width][heigth];
 
+        System.out.println("  1 2 3");
+        for (int i = 0; i < 3; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
