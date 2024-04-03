feef56a27cc98de0eb4e9d3d093c020af2666907
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-03 13:39:36 +0200

    add Book class with Attributes,Constructor,Getter e Setter

---
+++ src/www/experis/library/Book.java
@@ -0,0 +1,48 @@
+package www.experis.library;
+
+public class Book {
+    // Attributes
+    private String title, author, editor;
+    private int numPage;
+
+    // Constructor
+    public Book(String title, String author, String editor, int numPage) {
+        this.title = title;
+        this.author = author;
+        this.editor = editor;
+        this.numPage = numPage;
+    }
+
+    // Getter e Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public int getNumPage() {
+        return numPage;
+    }
+
+    public void setNumPage(int numPage) {
+        this.numPage = numPage;
+    }
+}
