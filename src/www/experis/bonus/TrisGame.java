f3b9cfbce886be39c7597789c4aa9c30b33e2be6
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-03 17:08:30 +0200

    init TrisGame + width structure

---
+++ src/www/experis/bonus/TrisGame.java
@@ -0,0 +1,14 @@
+package www.experis.bonus;
+
+public class TrisGame {
+    public static void main(String[] args) {
+        final int width = 3;
+        final int heigth = 3;
+        for (int i = 0; i < width; i++) {
+            System.out.print(" " + (i+1));
+            for (int j = 0; j < heigth; j++) {
+
+            }
+        }
+    }
+}
